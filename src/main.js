2372c40c0378e7116b1da73e2e658906e14271eb
David Gaspar <davidgaspar.dev@gmail.com>
2024-09-25 01:04:39 -0300

    feat: Implement escpos dependency for printer functionality

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow, globalShortcut, ipcMain } = require('electron');
 const path = require('path');
+const escpos = require('escpos');
 
 const createScreen = () => {
     const screen = new BrowserWindow({
@@ -42,6 +43,63 @@ app.on('will-quit', () => {
     globalShortcut.unregisterAll();
 });
 
+// Example from message:
+// {
+//     "name": "test test",
+//     "methodology": "VIVA LEVE",
+//     "level": 2,
+//     "stage": 6,
+//     "needs": [
+//     {
+//         "name": "Qualidade de Vida",
+//         "priority": 60,
+//         "showName": "Melhorar a qualidade de vida"
+//     }
+//     ],
+//     "date": "2024-09-25T03:05:14.238Z",
+//     "specialNeeds": [
+//     {
+//         "name": "Qualidade de Vida",
+//         "priority": 60,
+//         "showName": "Melhorar a qualidade de vida"
+//     }
+//     ]
+// }
 ipcMain.on('printer-result', (_, message) => {
     console.log('main process:', message);
+
+    const device = new escpos.Network('10.0.6.2', 9100);
+    const options = { encoding: 'GB18030' /* default */ };
+    const printer = new escpos.Printer(device, options);
+
+    device.open((error) => {
+        if (error) {
+            console.error(error);
+            return;
+        }
+
+        printer
+            .font('a')
+            .align('ct')
+            .style('bu')
+            .size(1, 1)
+            .text(`Nome: ${message.name}`)
+            .text(`Metodologia: ${message.methodology}`)
+            .text(`Nível: ${message.level}`)
+            .text(`Fase: ${message.stage}`);
+        
+        printer.feed(1);
+
+        if (message.specialNeeds && message.specialNeeds.length > 0) {
+            printer.text('Necessidades especiais:');
+
+            message.specialNeeds.forEach((need) => {
+                printer.text(`> ${need.showName}`);
+            });
+        }
+
+        printer.feed(1);
+        printer.cut();
+        printer.close();
+    });
 });
--- src/preload.js
+++ src/preload.js
@@ -3,28 +3,6 @@ const { contextBridge, ipcRenderer } = require('electron');
 function preloadSetup() {
     contextBridge.exposeInMainWorld('MessageInvoker', {
         // Expose a method to the web page to send messages to the main process
-        // Example from message:
-        // {
-        //     "name": "test test",
-        //     "methodology": "VIVA LEVE",
-        //     "level": 2,
-        //     "stage": 6,
-        //     "needs": [
-        //     {
-        //         "name": "Qualidade de Vida",
-        //         "priority": 60,
-        //         "showName": "Melhorar a qualidade de vida"
-        //     }
-        //     ],
-        //     "date": "2024-09-25T03:05:14.238Z",
-        //     "specialNeeds": [
-        //     {
-        //         "name": "Qualidade de Vida",
-        //         "priority": 60,
-        //         "showName": "Melhorar a qualidade de vida"
-        //     }
-        //     ]
-        // }
         postMessage: (jsonMessage) => {
             const message = JSON.parse(jsonMessage);
 
